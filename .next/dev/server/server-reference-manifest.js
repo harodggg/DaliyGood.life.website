80d8fa96dd4f6cb124a6d08e32e3fc2cac37ff11
harodggg <haroldtiansheng@gmail.com>
2025-11-25 01:31:51 +0800

    fix: fix some buges

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aMv0iCf3qcrmPX4RE4oxNu319ORlUJOc90al3atRCGk=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zz7wQTExRyCoRCctusBvOBxJWPUbxBY4+5NMgFjz/lg=\"\n}"
