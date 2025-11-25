1f098ebc86e39b3a3218ec9f49d946e43225ee86
harodggg <haroldtiansheng@gmail.com>
2025-11-25 18:43:25 +0800

    feat: add social
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Daliy Good Life</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       </Head>
       <main>
         <h1 className={styles.title}>Welcome to Daliy Good Life!</h1>
@@ -18,6 +19,19 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <p>© 2024 Daliy Good Life. All rights reserved.</p>
+        <div class="social-links-container">
+          <a href="您的 GitHub 链接" target="_blank" class="social-icon github" title="在 GitHub 上查看代码">
+            <i class="fa-brands fa-github"></i>
+          </a>
+
+          <a href="您的 Discord 链接" target="_blank" class="social-icon discord" title="加入我们的 Discord 社区">
+            <i class="fa-brands fa-discord"></i>
+          </a>
+
+          <a href="您的 Telegram 链接" target="_blank" class="social-icon telegram" title="订阅我们的 Telegram 频道">
+            <i class="fa-brands fa-telegram-plane"></i>
+          </a>
+        </div>
       </footer>
 
     </div>
